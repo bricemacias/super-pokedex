f7516d769b4592b465f6e2aa13898dec3a7e45ef
Brice Mvf <bricemacias@hotmail.com>
2019-09-24 20:01:49 +0200

    Unsaved Changes commited

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import CardList from './components/CardList';
+import SearchBox from './components/SearchBox';
 import { pokemons } from './pokemons-example';
 
 function App() {
   return (
     <div className="App">
       <h1>Super Pokedex</h1>
-      <div className="CardList">
-        <CardList pokemons={pokemons} />
-      </div>
+      <CardList className="CardList" pokemons={pokemons} />
     </div>
   );
 }
