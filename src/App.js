f4673f0ed74d4e9da93d1c918fd88a4287b5dbe0
Brice Mvf <bricemacias@hotmail.com>
2019-09-24 20:21:51 +0200

    App component changed to stateful component and SearchBox component working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 import { pokemons } from './pokemons-example';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Super Pokedex</h1>
-      <SearchBox />
-      <CardList className="CardList" pokemons={pokemons} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemons: pokemons,
+      searchfield: ''
+    };
+  }
+
+  onSearchTyping = event => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredPokemons = this.state.pokemons.filter(pokemon => {
+      return pokemon.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="App">
+        <h1>Super Pokedex</h1>
+        <SearchBox searchChange={this.onSearchTyping} />
+        <CardList className="CardList" pokemons={filteredPokemons} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchChange }) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba br3 b--light-grey bg-near-white"
         type="search"
         placeholder="Search Pokemons"
+        onChange={searchChange}
       />
     </div>
   );
