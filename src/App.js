f0dd9260fc09f3cb49adc86b6190d3d859e3500e
Brice Mvf <bricemacias@hotmail.com>
2019-09-24 20:07:56 +0200

    SearchBox component created and added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Super Pokedex</h1>
+      <SearchBox />
       <CardList className="CardList" pokemons={pokemons} />
     </div>
   );
---
+++ src/components/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = () => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba br3 b--light-grey bg-near-white"
+        type="search"
+        placeholder="Search Pokemons"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
