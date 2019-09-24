32976077860e41c9e1fc9c17016853090869bd5a
Brice Mvf <bricemacias@hotmail.com>
2019-09-24 21:28:32 +0200

    Data fetched online using axios and useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
+import axios from 'axios';
 import { pokemonsExample } from './pokemons-example';
 
 function App() {
-  const [pokemons] = useState(pokemonsExample);
+  const [pokemons, setPokemons] = useState(pokemonsExample);
   const [searchfield, setSearchField] = useState('');
+  //const [checkboxvalue, setCheckboxvalue] = useState('name');
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json'
+      )
+      .then(response => {
+        const fetchedPokemons = response.data.pokemon;
+        console.log(fetchedPokemons);
+        setPokemons(fetchedPokemons);
+      });
+  });
 
   const onSearchTyping = event => {
     setSearchField(event.target.value);
