f87e960f72f5cf15cb1c8f3d994696793ac7f8e0
Brice Mvf <bricemacias@hotmail.com>
2019-09-26 18:07:38 +0200

    build folder taken of from gitignore

---
+++ build/precache-manifest.e3109a1e4085d38bbe97a9443bedb6ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89564680b9a6525cd5830af9d581a6c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8fab59acd2ce05149b8",
+    "url": "/static/css/2.b4057b3c.chunk.css"
+  },
+  {
+    "revision": "366563404a51c6350d09",
+    "url": "/static/css/main.0b62fd28.chunk.css"
+  },
+  {
+    "revision": "b8fab59acd2ce05149b8",
+    "url": "/static/js/2.2fde542e.chunk.js"
+  },
+  {
+    "revision": "366563404a51c6350d09",
+    "url": "/static/js/main.e0a17e36.chunk.js"
+  },
+  {
+    "revision": "4fb026de23a31de353fd",
+    "url": "/static/js/runtime-main.576f3c65.js"
+  }
+]);
